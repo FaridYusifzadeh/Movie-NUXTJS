f4cd43f700e844870edbd97b0a13a82d88417e4a
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-15 09:41:58 +0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="app">
+    <Nuxt />
+  </div>
+</template>
+x``
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Hero />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  async fetch(){
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1"
+      );
+      const result = await data;
+      console.log(result.data);
+    },
+  },
+};
+</script>
