816b97d2ff835d61e2bc6b106aac777242910900
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-15 10:01:38 +0400

    add data

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,18 @@ export default {
       movies: [],
     };
   },
-  async fetch(){
-    await this.getMovies()
+  async fetch() {
+    await this.getMovies();
   },
   methods: {
     async getMovies() {
       const data = axios.get(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1"
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1`
       );
       const result = await data;
-      console.log(result.data);
+      result.data.results.forEach((item) => {
+        this.movies.push(item);
+      });
     },
   },
 };
